e0f1136b8834309193aa2da92583752b8c0b453a
Thomas Dinkgreve <thomasdinkgreve@gmail.com>
2022-02-03 16:25:44 +0100

    l

--- application.py
+++ application.py
@@ -33,7 +33,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///statiegelddoneren.db")
+db = SQL(os.getenv("DATABASE_URL", "sqlite:///statiegelddoneren.db"))
 
 # Send mail
 mail= Mail(app)
